2a2f9ee105cd4726407952f9cef722984c3a43e8
Mikhail Yudin <fagci.nsk@gmail.com>
2022-08-18 10:27:49 +0700

    elixir port scanner

---
+++ scan.ex
@@ -0,0 +1,17 @@
+#!/usr/bin/env elixir
+ip = '192.168.0.200'
+ports = 1..128
+
+check = fn port ->
+  with {:ok, s} <- :gen_tcp.connect(ip, port, [], 750) do
+    :gen_tcp.close(s)
+    port
+  end
+end
+
+ports
+|> Task.async_stream(check, max_concurrency: 128)
+|> Stream.map(&elem(&1, 1))
+|> Stream.filter(&is_number/1)
+|> Stream.each(&IO.puts/1)
+|> Stream.run()
