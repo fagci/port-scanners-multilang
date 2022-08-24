617ee9ae06b8156351c23798ec3d8a2b94d903c3
Mikhail Yudin <fagci.nsk@gmail.com>
2022-08-25 01:37:45 +0700

    feat: dart port scanner

---
+++ scan.dart
@@ -0,0 +1,20 @@
+#!/usr/bin/env dart
+
+import 'dart:async';
+import 'dart:io';
+
+const host = "192.168.0.200";
+const timeout = Duration(milliseconds: 750);
+
+Future check(port) async {
+  return Socket.connect(host, port, timeout: timeout).then((s) {
+    print(s.remotePort);
+    s.destroy();
+  }).catchError((e) => null);
+}
+
+Future main() async {
+  var ports = new List<int>.generate(128, (i) => i + 1);
+  var futures = ports.map(check);
+  return Future.wait(futures);
+}
