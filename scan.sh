9ccb00ecd46c0845d27a3d1f200dd606d9b2c638
Mikhail <fagci.nsk@gmail.com>
2023-01-21 15:05:17 +0700

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-seq 1 128 \
-    | xargs -I@ -P128 timeout 0.75 bash -c \
-    '(> /dev/tcp/192.168.0.200/@) 2> /dev/null && echo @'
+seq 1 128 | xargs -I@ -P128 timeout 0.75 bash -c '(> /dev/tcp/192.168.0.200/@) 2> /dev/null && echo @'
