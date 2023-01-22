9a086d5d692d201b6299eb28d52a70ae17f92920
Mikhail Yudin <fagci.nsk@gmail.com>
2023-01-23 02:41:54 +0700

    simplify

--- scan.mjs
+++ scan.mjs
@@ -5,15 +5,15 @@ import { Socket } from 'net'
 const HOST = '192.168.0.200';
 
 async function check(host, port) {
-    let socket = new Socket().setTimeout(750);
+    const socket = new Socket().setTimeout(750);
 
-    socket.on('timeout', socket.destroy);
-    socket.on('error', socket.destroy);
-
-    socket.connect(port, host, function() {
-        this.destroy();
-        console.log(port);
-    });
+    socket
+        .on('timeout', socket.destroy)
+        .on('error', socket.destroy)
+        .connect(port, host, function() {
+            this.destroy();
+            console.log(port);
+        });
 };
 
 const tasks = [...Array(128)].map((_, i) => check(HOST, i + 1));
