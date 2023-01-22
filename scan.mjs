556122d18d753ae23765b4d638cbd2543d102f21
Mikhail Yudin <fagci.nsk@gmail.com>
2023-01-23 02:16:10 +0700

    mv to es6 module

--- scan.mjs
+++ scan.mjs
@@ -1,11 +1,11 @@
 // true 2>/dev/null; exec /usr/bin/env node "$0" "$@"
 
-const net = require('net');
+import { Socket } from 'net'
 
 const HOST = '192.168.0.200';
 
 async function check(host, port) {
-    let socket = new net.Socket();
+    let socket = new Socket();
 
     socket.on('timeout', socket.destroy);
     socket.on('error', socket.destroy);
@@ -19,6 +19,4 @@ async function check(host, port) {
 
 const tasks = [...Array(128)].map((_, i) => check(HOST, i + 1));
 
-(async () => {
-    for await (const _ of tasks);
-})();
+for await (const _ of tasks);
